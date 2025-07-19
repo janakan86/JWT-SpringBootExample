e899d3f303672d4a79e7af7790ed99f766a6ebfe
Kanag Janakan <janakan86@gmail.com>
2025-07-19 15:04:36 +1000

    Renamed file
    Renamed file name

--- src/main/java/com/example/jwtExample/config/SecurityConfig.java
+++ src/main/java/com/example/jwtExample/config/SecurityConfig.java
@@ -1,34 +1,30 @@
 package com.example.jwtExample.config;
 
 
-import com.example.jwtExample.filter.AuthenticationFilter;
+import com.example.jwtExample.filter.JWTAuthenticationFilter;
 import com.example.jwtExample.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     UserDetailsServiceImpl userDetailsService;
-    AuthenticationFilter authenticationFilter;
+    JWTAuthenticationFilter JWTAuthenticationFilter;
 
-    public SecurityConfig(UserDetailsServiceImpl userDetailsService,AuthenticationFilter authenticationFilter){
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService, JWTAuthenticationFilter JWTAuthenticationFilter){
         this.userDetailsService = userDetailsService;
-        this.authenticationFilter = authenticationFilter;
+        this.JWTAuthenticationFilter = JWTAuthenticationFilter;
     }
 
     @Bean
@@ -50,7 +46,7 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.POST, "/login").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/testAdminPage").hasRole("ADMIN")
                                 .anyRequest().authenticated())
-                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(JWTAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
